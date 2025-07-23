a615cffd466f69460ec60abdc6a596ada84c0788
”simobeijer” <beijer.simon@gmail.com>
2025-07-24 00:38:54 +0200

    test deploy again

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'sha256-jqiZxOcKsKr0ZT2GYz3fP8tcJd3k4Q7eAPIYCMJvWRg=' 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo=' 'sha256-QraP4ffA8dfGeAcot2+KUXke/YDBBm8mia6e6diV8ZI=' 'sha256-X0mnlJEBzgN8D2MswzyPzcr0ICoiOVsg6ZMqidbYkWs=' 'sha256-1DOyrsWHOrOvJQJ7+1AYX4jYzXD9DQMOoBXTnb/A11I=' 'sha256-UQM/A8/ckK3LLU3SxlplcqrTTKTyjVQCA9GkV7KZnlE=' 'sha256-iYXd8UZaitghZQV6m0eDyAHLJutUoMe39DYpdfU1zQI=' va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' generativelanguage.googleapis.com va.vercel-scripts.com;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' generativelanguage.googleapis.com va.vercel-scripts.com;"
           },
         ],
       },
